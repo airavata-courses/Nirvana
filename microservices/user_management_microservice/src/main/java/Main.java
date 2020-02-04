7963f8c2da51c8a0873ed86f2980e3e5de5403a2
nahushr <nahushrai@gmail.com>
2020-02-04 18:11:34 -0500

    Issue #6
    Issue #6
    
    Co-Authored-By: Raj Nandu <rajnandu2711@gmail.com>

---
+++ microservices/user_management_microservice/src/main/java/Main.java
@@ -0,0 +1,8 @@
+import authentication.AuthConsumer;
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        AuthConsumer authConsumer = new AuthConsumer();
+        authConsumer.runConsumer();
+    }
+}
