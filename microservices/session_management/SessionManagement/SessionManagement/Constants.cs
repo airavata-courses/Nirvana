cadc6ab604dd0527602d2d9d254f3d80bcfbf345
nahushr <nahushrai@gmail.com>
2020-02-05 21:17:06 -0500

    Revert "Revert "Issue #9""
    This reverts commit 384a46cf7d739085273d070990face0e3a5a1909.

---
+++ microservices/session_management/SessionManagement/SessionManagement/Constants.cs
@@ -0,0 +1,30 @@
+﻿using Confluent.Kafka;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SessionManagement
+{
+    public class Constants
+    {
+        // mysql/mariadb variables
+        public static String serverEndPoint = "localhost";
+        public static String serverUserName = "root";
+        public static String serverPassword = "mariadb123";
+        public static String serverPort = "3306";
+        public static String serverDatabaseName = "sessionmanagement";
+
+        // kafka endpoint variables
+        public static String kafkaEndPoint = "localhost:9092";
+        public static String kafkaGroupId = "kafkagroupid";
+        public static String kafkaConsumerTopics = "session_management_and_logging_service";
+        public static ConsumerConfig consumerConfig = new ConsumerConfig
+        {
+            GroupId = Constants.kafkaGroupId,
+            BootstrapServers = Constants.kafkaEndPoint,
+            AutoOffsetReset = AutoOffsetReset.Earliest
+        };
+
+
+    }
+}
