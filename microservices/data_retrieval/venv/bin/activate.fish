87f215fa5eb817e56f0494be89a10f9de981a8ab
Raj <juhideshkar@gmail.com>
2020-02-12 00:58:48 -0500

    #15

--- microservices/data_retrieval/venv/bin/activate.fish
+++ microservices/data_retrieval/venv/bin/activate.fish
@@ -29,7 +29,7 @@ end
 # unset irrelevant variables
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/home/raj/Desktop/Data/Nirvana/Nirvana/microservices/data_retrieval/venv"
+set -gx VIRTUAL_ENV "/home/raj/Desktop/Data/Nirvana/Data_Retrieval_Service_Branch/Nirvana/microservices/data_retrieval/venv"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
