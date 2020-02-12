ff236ea5fe4b9fd94166ad1eaa580749d69ad661
nahushr <nahushrai@gmail.com>
2020-02-12 01:03:14 -0500

    Issue #16
    Issue #16

--- api_gateway/controllers/data_controller.py
+++ api_gateway/controllers/data_controller.py
@@ -14,7 +14,11 @@ def get_data_thread():
 def retrieve_data():
     try:
         # Fetching data from post body
+<<<<<<< Updated upstream
         data = request.args
+=======
+        data = request.json
+>>>>>>> Stashed changes
         headers = request.headers
         payload = JwtHandler.decode_auth_token(headers['Authorization'])
         if not payload:
@@ -30,10 +34,48 @@ def retrieve_data():
             if generated_map_key in getdata():
                 consumer_value = getdata()[generated_map_key]
                 deletedata(generated_map_key)
+<<<<<<< Updated upstream
                 return jsonify({'ApiCall': str(consumer_value, 'utf-8')}), 200
+=======
+                return jsonify({'ApiCall': str(consumer_value, 'utf-8')}), 500
+>>>>>>> Stashed changes
             counter += 1
             if (counter > 6):
                 return jsonify({'ApiCall': "request timeout"}), 500
             time.sleep(5)
     except Exception as e:
         return jsonify({'Error': str(e)}), 500
+<<<<<<< Updated upstream
+=======
+
+@data_api.route("/retrieveDataViz", methods=['GET'])
+def retrieve_data_viz():
+    try:
+        data = request.json
+        headers = request.headers
+        # payload = JwtHandler.decode_auth_token(headers['Authorization'])
+        # if not payload:
+        #     return jsonify({'error': "JWT invalid"})
+
+        topic = "retrieve_data_viz_service"
+        generated_map_key = generate_keys_for_user(headers['email'], topic)
+        producer.send(topic,
+                      key=bytes(generated_map_key, 'utf-8'), value=data)
+        thread1 = threading.Thread(target=get_data_thread)
+        thread1.start()
+        counter = 0
+        while True:
+            if generated_map_key in getdata():
+                consumer_value = getdata()[generated_map_key]
+                deletedata(generated_map_key)
+                return jsonify({'ApiCall': str(consumer_value, 'utf-8')}), 500
+            counter += 1
+            if (counter > 6):
+                return jsonify({'ApiCall': "request timeout"}), 500
+            time.sleep(5)
+    except Exception as e:
+        return jsonify({'Error': str(e)}), 500
+
+
+
+>>>>>>> Stashed changes
