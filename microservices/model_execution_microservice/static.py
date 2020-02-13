11af184f9f853ee72c7c6e7ea427d4c02c9f13f3
nahushr <nahushrai@gmail.com>
2020-02-12 20:39:42 -0500

    Issue #17
    Issue #17

---
+++ microservices/model_execution_microservice/static.py
@@ -0,0 +1,12 @@
+import json
+from kafka import KafkaConsumer
+from kafka import KafkaProducer
+from bson import json_util
+
+KAFKA_HOSTS = ['localhost:9092']
+KAFKA_VERSION = (0, 10)
+consumer = KafkaConsumer("retrieve_data_future_service", auto_offset_reset='earliest', group_id='kafkagroupid',
+                         enable_auto_commit=True,
+                         bootstrap_servers=KAFKA_HOSTS, api_version=KAFKA_VERSION)
+producer = KafkaProducer(bootstrap_servers=KAFKA_HOSTS, api_version=KAFKA_VERSION,
+                         value_serializer=lambda m: json.dumps(m, default=json_util.default).encode('utf-8'))
