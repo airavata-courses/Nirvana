18fe37287d5f171605c2eca1c1dcd36fac5e47f6
Raj Nandu <rajnandu2711@gmail.com>
2020-05-01 17:48:50 -0400

    Update static.py

--- microservices/Microservice_Model Execution/app/static.py
+++ microservices/Microservice_Model Execution/app/static.py
@@ -3,7 +3,7 @@ from kafka import KafkaConsumer
 from kafka import KafkaProducer
 from bson import json_util
 
-KAFKA_HOSTS = ['kafka:9092']
+KAFKA_HOSTS = ['kafka1:9092']
 
 KAFKA_VERSION = (0, 10)
 consumer = KafkaConsumer("retrieve_data_future_service", auto_offset_reset='earliest', group_id='kafkagroupid',
